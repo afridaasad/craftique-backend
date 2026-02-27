743730670a6aee629758d5088c6c2e86a61da941
afridaasad <afreedaasad.1808@gmail.com>
2026-02-28 00:49:42 +0530

    made swagger

--- craftique/settings.py
+++ craftique/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "api",
     "django_filters",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -143,12 +144,22 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_FILTER_BACKENDS": ["django_filters.rest_framework.DjangoFilterBackend"],
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 from datetime import timedelta
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": True,
+    "BLACKLIST_AFTER_ROTATION": True,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+}
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Craftique API",
+    "DESCRIPTION": "Multi-role marketplace backend",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
 }
 
 INSTALLED_APPS += ["rest_framework_simplejwt.token_blacklist"]
